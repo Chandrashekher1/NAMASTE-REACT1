73e6d2cc60ad15547f75996225cd2f56e0d5e17a
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-08-29 23:57:27 +0530

    add Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Chandrashekher Prasad</h1>
+            <h2>Contact No : 9990418622</h2>
+        </div>
+    )
+}
+export default Contact
